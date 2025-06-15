d2fee8bddb7c3c636d82b939d4e89c1f3f149f16
nayan <nayannomm@gmail.com>
2025-06-15 17:28:06 +0530

    barlow font fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Inter, Geist, Geist_Mono } from "next/font/google";
+import { Inter, Geist, Geist_Mono, Barlow_Condensed } from "next/font/google";
 import "./globals.css";
 
 // Load fonts with CSS variables
@@ -9,13 +9,19 @@ const geistSans = Geist({
 });
 
 const inter = Inter({
-  subsets: ["latin"],
   variable: "--font-inter",
+  subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+});
+
+const barlowCondensed = Barlow_Condensed({
+  variable: "--font-barlow-condensed",
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"], // Optional: adjust as needed
 });
 
 export const metadata: Metadata = {
@@ -31,7 +37,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.variable} ${geistSans.variable} ${geistMono.variable} font-sans text-[12px] antialiased`}
+        className={`${inter.variable} ${geistSans.variable} ${geistMono.variable} ${barlowCondensed.variable} font-sans text-[12px] antialiased`}
       >
         {children}
       </body>
