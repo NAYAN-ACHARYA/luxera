44c706ced507a8821c942684d9eb1056f149405e
nayan <nayannomm@gmail.com>
2025-06-19 00:07:06 +0530

    globalcss updated

--- app/footwear/page.tsx
+++ app/footwear/page.tsx
@@ -4,10 +4,10 @@ export default function Footwear() {
   return (
     <>
       <Header />
-      <div className="flex flex-col md:flex-row items-center justify-between px-6 py-10 gap-6 bg-[#fefaf6]  font-[Barlow_Condensed] mt-[140px] md:max-h-96">
+      <div className="flex flex-col md:flex-row items-center justify-between px-6 py-10 gap-6 bg-[#fefaf6] font-[Barlow_Condensed] mt-[140px] md:max-h-96">
         {/* Left Content */}
-        <div className="md:w-[25%]  w-full md:text-right md:ml-50 text-center">
-          <h2 className="text-6xl font-bold mb-4">Sneakers</h2>
+        <div className="md:w-[25%] w-full md:text-right text-center">
+          <h2 className="text-6xl font-bold mb-4 text-black">Sneakers</h2>
           <p className="text-gray-700 text-[26px]">
             Discover our sneakers designed from a rigorous selection of natural and recycled materials: recycled wool, hemp, linen and recycled cotton.
           </p>
@@ -15,13 +15,12 @@ export default function Footwear() {
 
         {/* Right Image */}
         <div className="md:w-[50%] w-full">
-  <img
-    src="/footwear-2.jpg" // Replace with actual path
-    alt="Sneakers"
-    className="w-full max-h-96 rounded-lg shadow-md object-cover"
-  />
-</div>
-
+          <img
+            src="/footwear-2.jpg"
+            alt="Sneakers"
+            className="w-full max-h-96 rounded-lg shadow-md object-cover"
+          />
+        </div>
       </div>
     </>
   );
